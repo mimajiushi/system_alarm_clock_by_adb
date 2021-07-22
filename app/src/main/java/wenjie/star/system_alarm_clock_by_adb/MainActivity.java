01d72f7758aafcbad234dd6c390fe7d58ecf058e
chenwenjie <chenwenjie.star@bytedance.com>
2021-07-22 20:49:03 +0800

    增加无界面activity，调试时无需再改代码

--- app/src/main/java/wenjie/star/system_alarm_clock_by_adb/MainActivity.java
+++ app/src/main/java/wenjie/star/system_alarm_clock_by_adb/MainActivity.java
@@ -1,42 +1,22 @@
 package wenjie.star.system_alarm_clock_by_adb;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
+import android.app.Activity;
 import android.os.Bundle;
-import android.provider.Settings;
-
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * 仅调试用
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        requestIgnoreBatteryOptimizations();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void requestIgnoreBatteryOptimizations() {
-        try{
-            Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-            intent.setData(Uri.parse("package:"+ getPackageName()));
-            startActivity(intent);
-
-            if (!Settings.canDrawOverlays(this)) {
-                //若未授权则请求权限
-                intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
-                intent.setData(Uri.parse("package:" + getPackageName()));
-                startActivityForResult(intent, 0);
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.finish();
     }
 }
